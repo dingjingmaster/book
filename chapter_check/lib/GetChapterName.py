afea7ece3a8644baf34405ee485ff11959deb12d
丁敬 <dingjing@live.cn>
2019-02-27 14:18:29 +0800

    update

--- chapter_check/lib/GetChapterName.py
+++ chapter_check/lib/GetChapterName.py
@@ -13,7 +13,7 @@ import re
 
 class GetChapterName:
     """ 第xxx章|节|幕 提取 """
-    step1 = ['章', '节', '辑', ' ', '\\s', ',', '，']
+    step1 = ['章', '节', '辑', ' ', '\\s', ',', '，', ':', '：']
     __step1_re = None
     """ 特殊字符 """
     step21 = ['【']
@@ -23,7 +23,7 @@ class GetChapterName:
     step3 = ['章', '节', '幕', ':', '：', ',', '，', '、', '\\d+', '\\S+ ']
     __step3_re = None
     """ 第 xxx(数字) 提取 """
-    step4 = ['第', '章']
+    step4 = ['第', '章', '(C|c)hapter\\d+']
     __step4_re = None
     """ 直接提取 """
     step5 = [
@@ -74,16 +74,16 @@ class GetChapterName:
         """ 第五种请情况 """
         tmp = ''
         fg = False
-        if not flag:
+        if '' == val:
             for k in cn:
                 if k in self.step5:
+                    tmp += k
                     if not fg:
                         fg = True
-                    tmp += k
                     continue
                 if fg:
-                    val = tmp
                     break
+            val = tmp
         """ 去除关键字(比较暴力) """
         val = val.replace('第', '')
         for m in set(self.step1) | set(self.step21) | set(self.step22) | set(self.step4) | set('.'):
@@ -93,10 +93,11 @@ class GetChapterName:
 
 if __name__ == '__main__':
     test = [
-        '11. 第九百九十九章 测试', '11.第 九百九十九 章 测试', '11. 第九百九十九 测试', '22. 第九百九十九　', '407.第 999章机遇',
+        '11. 第九百九十九章 测试', '11.第 九百九十九 章 测试', '11. 第九百九十九 测试', '22. 第九百九十九　', '407.第 999章机遇', '第1章： 悲惨山村',
         '11. 【九百九十九】 测试', '11.【999】测试',
         '11.999章测试', '11.999 章 测试', '248.九百九十九章 真假观沧海', '7.007 大当家的', '85.085.杀鸡儆猴', '444.第九百九十九.疑点重重', '236.第二百三十五进入皇宫的方法',
-        '11.第 999 测试', '11.第 999测试', '369.章九百九十九 偶遇'
+        '11.第 999 测试', '11.第 999测试', '369.章九百九十九 偶遇',
+        '2.二'
     ]
 
     cn = GetChapterName()
