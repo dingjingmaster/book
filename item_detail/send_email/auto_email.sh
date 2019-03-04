089dd086e33d860855b85730f7e9ee1db73286f9
丁敬 <dingjing@live.cn>
2019-03-04 11:27:04 +0800

    update

--- item_detail/send_email/auto_email.sh
+++ item_detail/send_email/auto_email.sh
@@ -13,7 +13,7 @@ email_send_to1="dingjing_ding@staff.easou.com"  #丁敬
 #email_send_to3="pm-r@staff.easou.com"
 #email_send_to4="xiadeqing_xia@staff.easou.com" # 夏德庆
 
-to="${email_send_to1}, ${email_send_to2}, ${email_send_to4}"
+to="${email_send_to1}, ${email_send_to2}, ${email_send_to3}, ${email_send_to4}"
 
 email_head='<head><style type="text/css"> p{font-weight:bold; font-size: 18px} td{border: 0px solid; background-color: #c0c0c0} table, tr{border: 0px solid grey;} th{border: 0px solid; background-color: #a8a8a8} </style></head><body><div>'
 email_buf=`cat ${content}`
